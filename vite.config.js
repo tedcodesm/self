d9d3612cc31ac49cde9ad324a2db5a43109e963f
tedcodesm <kaahenjoroge@gmail.com>
2025-04-07 10:02:31 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-],
-})
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "Resume",
+});
