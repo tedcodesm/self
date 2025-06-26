632e51e03c7f4a48f5972c805cdb321a7090906f
tedcodesm <kaahenjoroge@gmail.com>
2025-06-26 18:31:51 +0300

    work

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -32,7 +32,7 @@ const Portfolio = () => {
             modern design. Let's build something amazing together!"
           </p>
          <a
-  href="/Edward__kaahe.pdf"
+  href="/Resume_.pdf"
   download
   className="bg-lime-600 px-4 py-2 rounded-full text-white hover:bg-lime-700 transition"
 >
