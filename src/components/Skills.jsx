f6fb17a04ebe623015e2e90227cf0c5bb9bd11db
tedcodesm <kaahenjoroge@gmail.com>
2025-07-14 14:22:46 +0300

    skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,12 +8,18 @@ import {
   FaJs,
   FaNodeJs,
   FaReact,
+  FaPhp,
+  FaPython,
 } from "react-icons/fa";
 import {
   SiTailwindcss,
   SiReact,
   SiExpress,
   SiSocketdotio,
+  SiTypescript,
+  SiMysql,
+  SiVercel,
+  SiNextdotjs,
 } from "react-icons/si";
 
 const skills = [
@@ -24,9 +30,16 @@ const skills = [
   { Icon: FaDatabase, name: "MongoDB", level: 91, color: "#4DB33D" },
   { Icon: SiTailwindcss, name: "Tailwind CSS", level: 99, color: "#38BDF8" },
   { Icon: FaJs, name: "JavaScript", level: 87, color: "#F7DF1E" },
+  { Icon: SiTypescript, name: "TypeScript", level: 70, color: "#3178C6" },
+  { Icon: FaPhp, name: "PHP", level: 65, color: "#8892BE" },
+  { Icon: FaPython, name: "Python", level: 60, color: "#3776AB" },
+  { Icon: SiMysql, name: "MySQL", level: 68, color: "#00758F" },
+  { Icon: SiVercel, name: "Vercel", level: 80, color: "#000000" },
+  { Icon: SiNextdotjs, name: "Next.js", level: 73, color: "#000000" },
   { Icon: FaGitAlt, name: "Git", level: 90, color: "#F1502F" },
   { Icon: SiSocketdotio, name: "Socket.io", level: 50, color: "#010101" },
 ];
+
 
 const Skills = () => {
   return (
@@ -36,11 +49,11 @@ const Skills = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 0.5 }}
-        className="flex  md:h-screen flex-col items-center justify-center text-center px-6"
+        className="flex  md:h-full flex-col items-center justify-center text-center px-6"
       >
-        <h2 className="text-4xl tracking-wider font-bold mb-6">Skills</h2>
+        <h2 className="text-4xl tracking-wider font-bold mb-6 mt-4">Skills</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-xl">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full max-w-xl">
           {skills.map(({ Icon, name, level, color }, index) => (
             <motion.div
               key={index}
